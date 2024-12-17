d67cf0cdf7f9f832675b541d4e9550e7c506ad32
uma <uma@tigeen.ch>
2024-12-17 22:07:48 +0530

    sample data added' :

---
+++ src/sampledata/sampledata.js
@@ -0,0 +1,34 @@
+export const featuredTour = [
+  {
+    url: "https://burst.shopify.com/photos/beautiful-sunset-over-sea",
+
+    to: "Japan-South Korea",
+    package: "1,23,45,300",
+    nights: "10",
+    days: "11",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2016/11/18/17/25/ferris-wheel-1838419_960_720.jpg",
+
+    to: "scandinavia",
+    package: "3,45,12,300",
+    nights: "11",
+    days: "9",
+  },
+  {
+    url: "https://images.pexels.com/photos/1070972/pexels-photo-1070972.jpeg",
+
+    to: "UK - Europe",
+    package: "8,67,300",
+    nights: "4",
+    days: "5",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1604014073880-5b08b10a07b5",
+
+    to: "Eastern Europe",
+    package: "5,45,000",
+    nights: "9",
+    days: "8",
+  },
+];
