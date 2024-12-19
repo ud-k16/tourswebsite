373920e07be0cd4c0cc33e86d7ce97287dd79a18
uma <uma@tigeen.ch>
2024-12-19 12:41:29 +0530

    drawer added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,31 @@
 import styles from "./Header.module.css";
 import LOGO from "../../assests/logo.png";
 import { FiMenu } from "react-icons/fi";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 import { useEffect, useState } from "react";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
+import { TbLogin2 } from "react-icons/tb";
+import { GrGallery } from "react-icons/gr";
+import { GrContact } from "react-icons/gr";
 
 const Header = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth);
     window.addEventListener("resize", handleResize);
 
     // Clean up on component unmount
-    return () => window.removeEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
   return (
     <header className={styles.header}>
@@ -35,7 +49,43 @@ const Header = () => {
           </a>
         </nav>
       ) : (
-        <FiMenu size={60} />
+        <>
+          {!isOpen && <FiMenu onClick={toggleDrawer} size={60} />}
+          <Drawer open={isOpen} onClose={toggleDrawer} direction="right">
+            <nav className={styles.drawerItemContainer}>
+              <div className={styles.drawerItem}>
+                <IoHomeOutline size={40} />
+                <a href="/" className={styles.linkText}>
+                  Home
+                </a>
+              </div>
+              <div className={styles.drawerItem}>
+                <IoMdInformationCircleOutline size={40} />
+                <a href="/about" className={styles.linkText}>
+                  About
+                </a>
+              </div>
+              <div className={styles.drawerItem}>
+                <TbLogin2 size={40} />
+                <a href="/blog" className={styles.linkText}>
+                  Blog
+                </a>
+              </div>
+              <div className={styles.drawerItem}>
+                <GrGallery size={40} />
+                <a href="/gallery" className={styles.linkText}>
+                  Gallery
+                </a>
+              </div>
+              <div className={styles.drawerItem}>
+                <GrContact size={40} />
+                <a href="/contact" className={styles.linkText}>
+                  Contact
+                </a>
+              </div>
+            </nav>
+          </Drawer>
+        </>
       )}
     </header>
   );
