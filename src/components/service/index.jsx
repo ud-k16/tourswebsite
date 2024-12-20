5dc75e42dc2f318ce6a661764ecd8ceaa617676d
uma <uma@tigeen.ch>
2024-12-20 12:13:46 +0530

    name change'

--- src/components/service/index.jsx
+++ src/components/service/index.jsx
@@ -11,10 +11,10 @@ const Services = () => {
       <div className={styles.textSection}>
         <p className={styles.headingText}>Why Travel With us?</p>
         <hr></hr>
-        Skyplanet Holidays is the Best Travel Agency in Tamil Nadu and South
-        India, known for being one of the leading and most trusted tour and
-        travel agencies. We have served thousands of travel enthusiasts with the
-        best international group travel packages and customized travel packages,
+        XcelTours is the Best Travel Agency in Tamil Nadu and South India, known
+        for being one of the leading and most trusted tour and travel agencies.
+        We have served thousands of travel enthusiasts with the best
+        international group travel packages and customized travel packages,
         along with successful fixed departures.
       </div>
       <div className={styles.cardSection}>
